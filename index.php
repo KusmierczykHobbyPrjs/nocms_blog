079316982cb882501f4d126757c083304c8739d2
Tomasz Kusmierczyk <tomasz.kusmierczyk@gmail.com>
2021-02-21 12:49:13 +0200

    Bugfix page title

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
 <meta name="viewport" content="width=device-width, initial-scale=1.0"> 
 <link rel="stylesheet" type="text/css" href="style.css">
 <script src="actions.js"></script>
-<title>Tomasz Kuśmierczyk Misc Blog</title>
+<title>NoCMS Blog</title>
 
 <body onload="onLoad()" onresize="onResize()">
 
